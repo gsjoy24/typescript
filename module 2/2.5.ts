c21c13800b8a9c10d6c365949ebbb58b2635ab21
@u9KqBJdRPsTFXT <legalcoa668@gmail.com>
2023-11-05 00:11:47 +0600

    working

--- module 2/2.5.ts
+++ module 2/2.5.ts
@@ -12,4 +12,20 @@
 
 	type User = { id: number; name: string };
 	const result3 = createArrayGeneric<User>({ id: 121, name: 'Joy' });
+
+	// ! generics tuple
+	const createArrayWithTuple = <X, Y>(param1: X, param2: Y): [X, Y] => {
+		return [param1, param2];
+	};
+	const result4 = createArrayWithTuple<string, number>('Joy', 1212);
+
+	// !
+	const addStudent = <T>(student: T) => {
+		return {
+			...student,
+			isMota: true
+		};
+	};
+
+	const res1 = addStudent({ name: 'Joy', roll: 12 });
 }
