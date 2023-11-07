0ecfe09a462a7e3ec2dcee3a3dfdd1c1e95993fd
@u9KqBJdRPsTFXT <legalcoa668@gmail.com>
2023-11-07 22:06:42 +0600

    practicing

---
+++ module3/3.5.ts
@@ -0,0 +1,22 @@
+{
+	class BankAccount {
+		public readonly id: number; // this value will never changed!
+		public name: string;
+		private _balance: number; //this value can be access only in this class
+		constructor(id: number, name: string, balance: number) {
+			this.id = id;
+			this.name = name;
+			this._balance = balance;
+		}
+		addDeposit(amount: number) {
+			this._balance += amount;
+		}
+		getBalance() {
+			return this._balance;
+		}
+	}
+
+	const account = new BankAccount(12, 'Asif', 20);
+	account.addDeposit(200);
+	console.log(account.getBalance());
+}
