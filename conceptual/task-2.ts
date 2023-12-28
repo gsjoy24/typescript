ff1d2e08bcee6ba4e992f8b4d08c1d0b85269a67
gsjoy24 <gour.joy24@gmail.com>
2023-12-29 03:17:08 +0600

    task 2 added

---
+++ conceptual/task-2.ts
@@ -0,0 +1,23 @@
+/*
+ * Your challenge is to create a TypeScript function that turns an array of key-value pairs into a neatly organized object. Weave a function that seamlessly merges the keys and values from the array, transforming them into a structured object.
+
+* Once your function is crafted, apply it to an array filled with various key-value pairs. Witness the array's shift into a tidy and structured object, showcasing its newfound form.
+ */
+
+const keyValArr: [string, any][] = [
+	['id', 1],
+	['name', 'Joy'],
+	['age', 111],
+	['address', 'Rajshahi']
+];
+
+const getObj = <T extends string, U>(arr: [T, U][]): Record<T, U> => {
+	const transformedObj = arr.reduce((obj, [key, val]) => {
+		obj[key] = val;
+		return obj;
+	}, {} as Record<T, U>);
+
+	return transformedObj;
+};
+
+console.log(getObj(keyValArr));
